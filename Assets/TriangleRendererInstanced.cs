8d85db11b9a6396127649e7fb6c5a4a459c94f4b
Bernhard Rainer <bernhard.rainer@tuwien.ac.at>
2017-03-02 16:33:36 +0100

    simple subdivision

--- Assets/TriangleRendererInstanced.cs
+++ Assets/TriangleRendererInstanced.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TriangleRendererInstanced : MonoBehaviour {
 
@@ -30,42 +31,109 @@ public class TriangleRendererInstanced : MonoBehaviour {
         p2 -= s;
         var p3 = new Vector3(0, tetrahedronHeight, 0);
         
-        var vertices = new List<Vector3>()
-        {
-            p0, 
-            p1, 
-            p2, 
-            p3                                  
-        };
+        //var vertices = new List<Vector3>()
+        //{
+        //    p0, 
+        //    p1, 
+        //    p2, 
+        //    p3                                  
+        //};
                                 
-        var indices = new int[] {
-            //floor
-            0, 1, 2,
-            // Side 1
-            1, 0, 3,
-            // Side 2
-            2, 1, 3,
-            // Side 3
-            0, 2, 3 };
-        mesh.SetVertices(vertices);
-        mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+        //var indices = new int[] {
+        //    //floor
+        //    0, 1, 2,
+        //    // Side 1
+        //    1, 0, 3,
+        //    // Side 2
+        //    2, 1, 3,
+        //    // Side 3
+        //    0, 2, 3
+        //};
 
-        var cubeObj = GameObject.Find("Cube");
-       // mesh = cubeObj.GetComponent<MeshFilter>().mesh;
-        cubeObj.SetActive(false);
-       
+
+        var triangles = new List<Triangle>()
+        {
+            new Triangle(p0, p1, p2, Color.red),
+            new Triangle(p1, p0, p3, Color.red),
+            new Triangle(p2, p1, p3, Color.red),
+            new Triangle(p0, p2, p3, Color.red),
+        };
+
+        triangles = Subdivide(Subdivide(Subdivide(Subdivide(triangles))));
+
+        var vertices    = ToVertexList(triangles);
+        var colors      = ToColorList(triangles);
+        var numVertices = vertices.Count;
+
+
+        mesh.SetVertices(vertices);
+        mesh.SetColors(colors);
+        mesh.SetTriangles(Enumerable.Range(0, numVertices).ToArray(), 0);
+        
+        //mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+             
     }
-	 
+	
+
+    public static List<Vector3> ToVertexList(List<Triangle> triangles)
+    {
+        var vertices = new List<Vector3>();
+        triangles.ForEach(t => { vertices.Add(t.p0); vertices.Add(t.p1); vertices.Add(t.p2); });
+        return vertices;
+    }
+
+    public static List<Color> ToColorList(List<Triangle> triangles)
+    {
+        var colors = new List<Color>();
+        triangles.ForEach(t => { colors.Add(t.color); colors.Add(t.color); colors.Add(t.color); });
+        return colors;
+    }
+
+    public struct Triangle
+    {
+        public Vector3 p0, p1, p2;
+        public Color color;
+        
+        public Triangle(Vector3 p0, Vector3 p1, Vector3 p2, Color color)
+        {
+            this.p0 = p0;
+            this.p1 = p1;
+            this.p2 = p2;
+            this.color = color;
+        }
+    }
+
+
+    private List<Triangle> Subdivide(List<Triangle> triangles)
+    {
+        var newTriangles = new List<Triangle>();
+        triangles.ForEach(t =>
+        {
+            var p01 = (t.p0 + t.p1) / 2;
+            var p12 = (t.p1 + t.p2) / 2;
+            var p02 = (t.p2 + t.p0) / 2;
+
+            // Emit 3 triangles lying at the cornser of the old triangle
+            newTriangles.Add(new Triangle(t.p0, p01, p02, Color.red));
+            newTriangles.Add(new Triangle(t.p1, p12, p01, Color.red));
+            newTriangles.Add(new Triangle(t.p2, p02, p12, Color.red));
+            // Emit center triangle
+        });
+
+        return newTriangles;
+    }
+
     void Update()
     {
         //this.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
         //Debug.Log("ONRenderObject");
         var matrix0 = Matrix4x4.identity;
-       // material.
+        // material.
         //var matrix1 = matrix0 * Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 120, 0), Vector3.one);
         //var matrix2 = matrix0 * Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 240, 0), Vector3.one);
         //var matrix3 = matrix0 * Matrix4x4.TRS(Vector)
         Graphics.DrawMesh(mesh, matrix0, material, 0);
+        
         //Graphics.DrawMesh(mesh, transform.localToWorldMatrix * matrix1, material, 0);
         //Graphics.DrawMesh(mesh, transform.localToWorldMatrix * matrix2, material, 0);
 
